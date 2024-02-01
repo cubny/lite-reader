4c3fecd1d3afc32bf0937447e9cc5b51e3189f93
Alireza Eliaderani <172368+cubny@users.noreply.github.com>
2024-02-01 22:26:33 +0100

    remove legacy code

--- legacy/app/db/install.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE IF NOT EXISTS item (is_new NUMERIC, desc BLOB, id INTEGER PRIMARY KEY, link TEXT, rss_id NUMERIC, title TEXT, dir TEXT);
-CREATE TABLE IF NOT EXISTS rss (updated_at TEXT, lang TEXT, id INTEGER PRIMARY KEY, desc TEXT, title TEXT, link TEXT, url TEXT);
